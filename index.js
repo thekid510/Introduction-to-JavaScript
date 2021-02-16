8d08c293f5c911723f7dd6a9241a404419bba901
thekid510 <76237067+thekid510@users.noreply.github.com>
2021-02-16 14:55:46 -0800

    finished task 2

--- index.js
+++ index.js
@@ -88,8 +88,10 @@ Do the following:
    3. Return the newly calculated age
 */
 
-function dogYears(/*add your code here*/){
-    /*add your code here*/
+function dogYears(humanYears){
+   var dogYears = 7*humanYears;
+
+    return humanYears * 7;
 }
 
 
